503d140e901cffe40717922d391d3238c625658a
Pnsadeghy <pnsadeghy@gmail.com>
2023-06-12 10:36:31 +0330

    Fix jump feel

--- modules/state_machine/StateCharacter.gd
+++ modules/state_machine/StateCharacter.gd
@@ -4,9 +4,9 @@ class_name StateCharacter
 
 # properties
 @export var move_speed := 200.0
-@export var jump_force := 330.0
-@export var fall_gravity_scale := 2000.0
-@export var jump_gravity_scale := 980.0
+@export var jump_force := 420.0
+@export var fall_gravity_scale := 1500.0
+@export var jump_gravity_scale := 1300.0
 @export var wall_gravity_scale := 10.0
 @export var air_jumps_amount := 1
 
